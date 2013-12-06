ac46d5d415e97d3b185cb1d2d046f6ab4cd00959
Alan Franzoni <username@franzoni.eu>
2013-12-06 14:38:06 +0100

    Allow user yum.conf override.

--- rpmbuild-in-docker.sh
+++ rpmbuild-in-docker.sh
@@ -3,6 +3,7 @@ set -e
 SRCRPM=$(ls /src/*.spec | head -n 1)
 yum-builddep ${SRCRPM}
 cp -t /docker-rpm-build-root/SOURCES -r /src/*
-cp -t /docker-rpm-build-root/yum.repos.d /src/*.repo || /bin/true
+# allow the user to overwrite our yum.conf if he likes, otherwise use .repo files.
+{ cp /src/yum.conf /etc ; echo "yum.conf was overriden, .repo files will be ignored" ; } || { cp -t /docker-rpm-build-root/yum.repos.d /src/*.repo || /bin/true ; }
 rpmbuild -bb $SRCRPM
 cp -r /docker-rpm-build-root/RPMS /src
