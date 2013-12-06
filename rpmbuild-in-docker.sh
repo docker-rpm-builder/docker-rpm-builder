8b1086360375f0c8d6f2d23d8b3f7d199c6443e2
Alan Franzoni <username@franzoni.eu>
2013-12-06 17:10:07 +0100

    Enh: better messaging

--- rpmbuild-in-docker.sh
+++ rpmbuild-in-docker.sh
@@ -4,6 +4,6 @@ SRCRPM=$(ls /src/*.spec | head -n 1)
 yum-builddep ${SRCRPM}
 cp -t /docker-rpm-build-root/SOURCES -r /src/*
 # allow the user to overwrite our yum.conf if he likes, otherwise use .repo files.
-{ cp /src/yum.conf /etc ; echo "yum.conf was overriden, .repo files will be ignored" ; } || { cp -t /docker-rpm-build-root/yum.repos.d /src/*.repo || /bin/true ; }
+{ cp /src/yum.conf /etc && echo "yum.conf was overriden, .repo files will be ignored" ; } ||  { { cp -t /docker-rpm-build-root/yum.repos.d /src/*.repo && echo "no repo was added" ; } || /bin/true ; }
 rpmbuild -bb $SRCRPM
 cp -r /docker-rpm-build-root/RPMS /src
