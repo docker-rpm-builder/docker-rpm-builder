bed7cbbeecfa18561af7ca4470b96ccb32808535
Alan Franzoni <username@franzoni.eu>
2014-03-14 13:02:42 +0100

    Enh: better logging and naming fixes

--- rpmbuild-in-docker.sh
+++ rpmbuild-in-docker.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-set -e
-SRCRPM=$(ls /src/*.spec | head -n 1)
-yum-builddep ${SRCRPM}
+set -ex
+echo "starting $0"
+SPEC=$(ls /src/*.spec | head -n 1)
 cp -t /docker-rpm-build-root/SOURCES -r /src/*
 # allow the user to overwrite our yum.conf if he likes, otherwise use .repo files.
-{ cp /src/yum.conf /etc && echo "yum.conf was overriden, .repo files will be ignored" ; } ||  { { cp -t /docker-rpm-build-root/yum.repos.d /src/*.repo && echo "no repo was added" ; } || /bin/true ; }
-rpmbuild -bb $SRCRPM
+{ cp /src/yum.conf /etc && echo "yum.conf was overriden" ; } 
+{ cp -t /docker-rpm-build-root/yum.repos.d /src/*.repo && echo "custom repo files were added" ; } 
+yum-builddep ${SPEC}
+rpmbuild -bb $SPEC
 cp -r /docker-rpm-build-root/RPMS /src
+echo "Done"
