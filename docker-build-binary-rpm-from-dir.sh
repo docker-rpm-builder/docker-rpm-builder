a1b13e046499b41ef4a1c7e9fdd201a53cb50928
Alan Franzoni <username@franzoni.eu>
2014-09-19 17:51:24 +0200

    Enh: better message

--- docker-build-binary-rpm-from-dir.sh
+++ docker-build-binary-rpm-from-dir.sh
@@ -2,10 +2,10 @@
 set -e
 USAGE="Usage:\n$0 IMAGETAG SRCDIR <...additional docker options..>\n"
 IMAGETAG=$1
-[ -n "$1" ] || { echo "Missing IMAGETAG param" ; echo -e $USAGE ; exit 1 ;}
+[ -n "$1" ] || { echo "ERROR: Missing IMAGETAG param" ; echo -e $USAGE ; exit 1 ;}
 shift 1
 SRCDIR=$1
-[[ -n "$1" && -d $1 && -r $1 && -x $1 && -w $1 ]] || { echo "Missing SRCDIR or wrong permissions - must be an rwx directory" ; echo -e $USAGE ; exit 1 ;}
+[[ -n "$1" && -d $1 && -r $1 && -x $1 && -w $1 ]] || { echo "ERROR: Missing SRCDIR or wrong permissions - must be an rwx directory" ; echo -e $USAGE ; exit 1 ;}
 shift 1
 SRCDIR=$(readlink -f ${SRCDIR})
 echo "Now building project from $SRCDIR on image $IMAGETAG"
