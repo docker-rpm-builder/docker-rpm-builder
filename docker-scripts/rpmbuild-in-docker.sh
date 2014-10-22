46dc8fb656bd57e8ae66105ab6f6fb16ed75a1df
Alan Franzoni <username@franzoni.eu>
2014-10-22 15:55:00 +0200

    Enh: drop to interactive shell if build fails (introduce a flag in a next version)

--- docker-scripts/rpmbuild-in-docker.sh
+++ docker-scripts/rpmbuild-in-docker.sh
@@ -5,7 +5,7 @@ SPEC=$(ls /src/*.spec | head -n 1)
 cp -t /docker-rpm-build-root/SOURCES -r /src/*
 [ -x /src/drb-pre ] && /src/drb-pre
 /docker-scripts/rpm-setup-deps.sh
-rpmbuild -bb $SPEC
+rpmbuild -bb $SPEC || /bin/bash
 [ -x /src/drb-post ] && /src/drb-post
 cp -fr /docker-rpm-build-root/RPMS /src
 chown -R $1 /src/RPMS
