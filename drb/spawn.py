dd0eddd250db6b6f9040d30092235f76c4906063
Alan Franzoni <username@franzoni.eu>
2015-01-08 20:15:23 +0100

    Fix: proper propagation of stdout and stderr

--- drb/spawn.py
+++ drb/spawn.py
@@ -12,12 +12,12 @@ class SpawnedProcessError(Exception):
         self.error = error
 
     def __str__(self):
-        return "Command '%s' returned non-zero exit status %d" % (self.cmd, self.returncode)
+        return "Command '%s' returned non-zero exit status %d:\n%s\n%s\n" % (self.cmd, self.returncode, self.output, self.error)
 
 def sp(cmdformatstring, *params, **kwargs):
     fullcmd = cmdformatstring.format(*params, **kwargs)
     logging.debug("Now executing:\n%s\n", fullcmd)
-    process = Popen(fullcmd, stderr=STDOUT, shell=True)
+    process = Popen(fullcmd, stdout=PIPE, stderr=PIPE, shell=True)
     output, error = process.communicate()
     retcode = process.poll()
     if retcode:
