abf5e7ce2b74fe78954b6899981cf73b2489bdf5
Alan Franzoni <username@franzoni.eu>
2015-01-29 04:18:31 +0100

    Fix: actually let the user pass additional options

--- drb/commands/srcrpm.py
+++ drb/commands/srcrpm.py
@@ -64,11 +64,12 @@ _logger = logging.getLogger("drb.commands.srcrpm")
 @click.argument("image", type=click.STRING)
 @click.argument("srcrpm", type=click.Path(exists=True, dir_okay=False, resolve_path=True))
 @click.argument("target_directory", type=click.Path(file_okay=False, resolve_path=True))
+@click.argument("additional_docker_options", type=click.STRING, nargs=-1)
 @click.option("--verify-signature", is_flag=True)
 @click.option("--bash-on-failure", is_flag=True)
 @click.option("--sign-with", nargs=1, type=click.Path(exists=True, dir_okay=False, resolve_path=True))
 @click.option("--always-pull", is_flag=True)
-def srcrpm(image, srcrpm, target_directory, verify_signature=False, bash_on_failure=False,
+def srcrpm(image, srcrpm, target_directory, additional_docker_options, verify_signature=False, bash_on_failure=False,
            sign_with=None, always_pull=False):
     _logger.info("Now building %(srcrpm)s on image %(image)s", locals())
     if not os.path.exists(target_directory):
@@ -78,7 +79,7 @@ def srcrpm(image, srcrpm, target_directory, verify_signature=False, bash_on_fail
     dockerscripts = getpath("drb/dockerscripts")
     srpms_temp = tempfile.mkdtemp("SRPMS")
     shutil.copy(srcrpm, srpms_temp)
-    additional_docker_options = set()
+    internal_docker_options = set()
     srcrpm_basename = os.path.basename(srcrpm)
     uid = os.getuid()
     gid = os.getgid()
@@ -86,11 +87,11 @@ def srcrpm(image, srcrpm, target_directory, verify_signature=False, bash_on_fail
 
     bashonfail = ""
     if bash_on_failure:
-        additional_docker_options.add("-i")
-        additional_docker_options.add("-t")
+        internal_docker_options.add("-i")
+        internal_docker_options.add("-t")
         bashonfail = "bashonfail"
 
-    additional_docker_options = " ".join(additional_docker_options)
+    internal_docker_options = " ".join(internal_docker_options)
     encoded_signature = provide_encoded_signature(sign_with)
 
     if always_pull:
@@ -100,6 +101,7 @@ def srcrpm(image, srcrpm, target_directory, verify_signature=False, bash_on_fail
                                     "GPG_PRIVATE_KEY": encoded_signature})
 
     try:
+        additional_docker_options += " " + internal_docker_options
         srpms_inner_dir = sp("{dockerexec} run {image} rpm --eval %{{_srcrpmdir}}", **locals()).strip()
         rpms_inner_dir = sp("{dockerexec} run {image} rpm --eval %{{_rpmdir}}", **locals()).strip()
         sp("{dockerexec} run {additional_docker_options} -v {dockerscripts}:/dockerscripts -v {srpms_temp}:{srpms_inner_dir} -v {target_directory}:{rpms_inner_dir}"
