8683306c3269c64f54e9fa2f9ecbeca3e072f089
Alan Franzoni <username@franzoni.eu>
2015-03-19 19:34:57 +0100

    Fix: don't echo newline

--- drb/dockerscripts/rpmbuild-srcrpm-in-docker.sh
+++ drb/dockerscripts/rpmbuild-srcrpm-in-docker.sh
@@ -2,7 +2,7 @@
 set -ex
 # verify: security implications.
 [ -z "$1" ] && { echo "Missing parameters"; /bin/false; }
-eval $(echo $1 | base64 -d)
+eval $(echo -n $1 | base64 -d)
 
 [ -z "${SRCRPM}" ] && { echo "Missing SRCRPM"; /bin/false; }
 [ -z "${CALLING_UID}" ] && { echo "Missing CALLING_UID"; /bin/false; }
