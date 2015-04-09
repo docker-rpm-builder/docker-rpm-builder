3e3be1efc703ae53a0d2e2f8bb31d81d8947fb02
Alan Franzoni <username@franzoni.eu>
2015-04-09 22:56:15 +0200

    Enh: add Ubuntu Trusty deb build script

---
+++ debuntu-build-scripts/build-trusty-package.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+install_dir="/opt/docker-rpm-builder"
+
+set -e
+[ -n "$1" ]
+DRB_VERSION="$1"
+apt-get -y update
+apt-get -y dist-upgrade
+apt-get -y install python python-setuptools python-virtualenv ruby ruby-dev rubygems-integration perl
+gem install fpm -v 1.3.3
+easy_install pipsi
+mkdir -p ${install_dir}/{bin,env}
+pipsi --home ${install_dir}/env --bin-dir ${install_dir}/bin install --python /usr/bin/python "docker-rpm-builder==${DRB_VERSION}"
+cd /usr/bin
+ln -s /opt/docker-rpm-builder/bin/docker-rpm-builder .
+cd /
+tar czvf drb.tar.gz /opt/docker-rpm-builder /usr/bin/docker-rpm-builder
+cd /host
+fpm -s tar -t deb --name "docker-rpm-builder" --version ${DRB_VERSION} --depends "lxc-docker >= 1.3" ../drb.tar.gz 
+chmod 666 *.deb
