c95684fb3d39922e15730810c6d2801fab9f1943
Alan Franzoni <username@franzoni.eu>
2015-05-12 00:51:41 +0200

    Fix: build script

--- debuntu-build-scripts/build-package.sh
+++ debuntu-build-scripts/build-package.sh
@@ -6,7 +6,7 @@ set -e
 DRB_VERSION="$1"
 apt-get -y update
 apt-get -y dist-upgrade
-apt-get -y install python python-setuptools python-virtualenv ruby ruby-dev rubygems-integration perl build-essential libffi-dev
+apt-get -y install python python-setuptools python-virtualenv ruby ruby-dev rubygems-integration build-essential libffi-dev
 gem install fpm -v 1.3.3
 easy_install pipsi
 mkdir -p ${install_dir}/{bin,env}
@@ -16,5 +16,5 @@ ln -s /opt/docker-rpm-builder/bin/docker-rpm-builder .
 cd /
 tar czvf drb.tar.gz /opt/docker-rpm-builder /usr/bin/docker-rpm-builder
 cd /host
-fpm -s tar -t deb --name "docker-rpm-builder" --version ${DRB_VERSION} --depends "lxc-docker >= 1.3" ../drb.tar.gz 
+fpm -s tar -t deb --name "docker-rpm-builder" --version ${DRB_VERSION} --depends "lxc-docker >= 1.3" --depends "wget" ../drb.tar.gz 
 chmod 666 *.deb
