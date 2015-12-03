0417ae9448a5fd4d289e1aacb45092cf84d29c29
Alan Franzoni <username@franzoni.eu>
2015-12-03 11:46:06 +0100

    Enh: better test name

--- drb/tempdir.py
+++ drb/tempdir.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from tempfile import mkdtemp
+from tempfile import mkdtemp, gettempdir
 from shutil import rmtree
 import logging
-from weakref import ref
+import sys
+import os
 
 class TempDir(object):
     """A self-destroying temporary directory.
@@ -12,16 +13,26 @@ class TempDir(object):
 
     Absolutely not threadsafe. Don't even think about it.
     """
-    def __init__(self, prefix="/tmp/drbtemp.XXXXX"):
+    def __init__(self, prefix="/tmp/drb-temp."):
         self._tempdir = mkdtemp(prefix=prefix)
         self._was_deleted = False
         self._log = logging.getLogger("TempDir")
+
+    @classmethod
+    def platformwise(cls):
+        """Returns a suitable temporary directory depending on the current platform.
+        This usually means something in /tmp for linux and something in $HOME for OSX -
+        since it's the home directory which gets mounted when using Kitematic/boot2docker
+        /Docker Machine"""
+        base_dir = os.path.expanduser("~") if sys.platform == "darwin" else gettempdir()
+        return cls(prefix=os.path.join(base_dir, "drb-temp."))
+
 
     @property
     def path(self):
         return self._tempdir
 
-    def _delete(self):
+    def delete(self):
         if not self._was_deleted:
             rmtree(self._tempdir, onerror=lambda *args, **kwargs: self._log.critical("Could not completely remove temporary directory %s", self._tempdir))
             self._was_deleted = True
@@ -31,6 +42,6 @@ class TempDir(object):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self._delete()
+        self.delete()
 
 
---
+++ drb/test/integration_test_dir_command.py
@@ -0,0 +1,96 @@
+from drb.spawn import SpawnedProcessError
+from unittest2 import TestCase
+from click.testing import CliRunner
+import os
+import sys
+
+from drb.tempdir import TempDir
+from drb.commands.dir import dir
+
+REFERENCE_IMAGE = "alanfranz/drb-epel-7-x86-64:latest"
+import logging
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+
+class TestDirCommand(TestCase):
+    def setUp(self):
+        self.runner = CliRunner()
+        self.src = TempDir.platformwise()
+        self.rpm = TempDir.platformwise()
+
+    def tearDown(self):
+        self.src.delete()
+        self.rpm.delete()
+
+
+    def test_dir_command_fails_if_sources_unavailable_and_downloadsources_not_enabled(self):
+        with open(os.path.join(self.src.path, "tmux.spec"), "wb") as f:
+            f.write(TMUX_SPEC)
+
+        self.assertRaises(SpawnedProcessError, self.runner.invoke, dir, [REFERENCE_IMAGE, self.src.path, self.rpm.path], catch_exceptions=False)
+
+
+
+
+
+TMUX_SPEC = """
+Name:           tmux
+Version:        1.6
+Release:        3%{?dist}
+Summary:        A terminal multiplexer
+
+Group:          Applications/System
+# Most of the source is ISC licensed; some of the files in compat/ are 2 and
+# 3 clause BSD licensed.
+License:        ISC and BSD
+URL:            http://sourceforge.net/projects/tmux
+Source0:        http://pkgs.fedoraproject.org/repo/pkgs/tmux/tmux-1.6.tar.gz/3e37db24aa596bf108a0442a81c845b3/tmux-1.6.tar.gz
+BuildRoot:      %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
+
+BuildRequires:  ncurses-devel
+BuildRequires:  libevent-devel
+
+%description
+tmux is a "terminal multiplexer."  It enables a number of terminals (or
+windows) to be accessed and controlled from a single terminal.  tmux is
+intended to be a simple, modern, BSD-licensed alternative to programs such
+as GNU Screen.
+
+%prep
+%setup -q
+
+%build
+%configure
+make %{?_smp_mflags} LDFLAGS="%{optflags}"
+
+%install
+rm -rf %{buildroot}
+make install DESTDIR=%{buildroot} INSTALLBIN="install -p -m 755" INSTALLMAN="install -p -m 644"
+
+%clean
+rm -rf %{buildroot}
+
+%post
+if [ ! -f %{_sysconfdir}/shells ] ; then
+    echo "%{_bindir}/tmux" > %{_sysconfdir}/shells
+else
+    grep -q "^%{_bindir}/tmux$" %{_sysconfdir}/shells || echo "%{_bindir}/tmux" >> %{_sysconfdir}/shells
+fi
+
+%postun
+if [ $1 -eq 0 ] && [ -f %{_sysconfdir}/shells ]; then
+    sed -i '\!^%{_bindir}/tmux$!d' %{_sysconfdir}/shells
+fi
+
+%files
+%defattr(-,root,root,-)
+%doc CHANGES FAQ NOTES TODO examples/
+%{_bindir}/tmux
+%{_mandir}/man1/tmux.1.*
+
+%changelog
+* Fri Aug 09 2013 Steven Roberts <strobert@strobe.net> - 1.6-3
+- Building for el6
+- Remove tmux from the shells file upon package removal (RH bug #972633)
+"""
+
