7c21603440404493aebcdc91ffe7a2e487112241
Alan Franzoni <username@franzoni.eu>
2016-01-26 23:32:59 +0100

    Cleanup

--- debuntu-build-scripts/dockerbuild.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-docker run -v $(pwd):/host "$@"
--- experiments/docker-build-srpm-from-srcdir.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-IMAGETAG=$1
-[ -n "$1" ] || { echo "Missing IMAGETAG param" ; exit 1 ;}
-SRCDIR=$2
-[ -n "$2" ] || { echo "Missing SRCDIR" ; exit 1 ;}
-docker run -v `pwd`:/docker-files -v ${SRCDIR}:/src -w /docker-files $IMAGETAG ./rpmbuild-srpm-in-docker.sh
