8db9e7c2b238b68e7c24fbec6c2bcfa83d95b31e
Alan Franzoni <username@franzoni.eu>
2016-02-26 18:02:44 +0100

    Enh: use an if/else block which actually lets us understand what's happening

--- drb/dockerscripts/rpmbuild-srcrpm-in-docker.sh
+++ drb/dockerscripts/rpmbuild-srcrpm-in-docker.sh
@@ -39,13 +39,38 @@ then
     KEYNAME="${BASH_REMATCH[1]}"
     [ -n "${KEYNAME}" ] || { echo "could not find key for signing purpose"; exit 1; }
     echo -e "%_gpg_name ${KEYNAME}\n%_signature gpg" > ${HOME}/.rpmmacros
-    rpmbuild_out="$(rpmbuild --rebuild ${RPMBUILD_OPTIONS} "${SRPMS_DIR}/${SRCRPM}" 2>&1)"
-    exitcode="$?"
-    [ "${exitcode}" -ne 0 ] && { [ "bashonfail" == "${BASH_ON_FAIL}" ] && { echo "Build failed, spawning a shell" ; /bin/bash ; exit 1; } || { rm -f ${files} ; /bin/false ; } ; }
-    files=$(sed -n -e '/Checking for unpackaged file/,$p' <<< "${rpmbuild_out}" | grep 'Wrote:' | cut -d ':' -f 2)
-    echo -e "\n" | setsid rpmsign --addsign ${files} || { [ "bashonfail" == "${BASH_ON_FAIL}" ] && { echo "Signing failed, spawning a shell" ; /bin/bash ; exit 1; } || { rm -f ${files} ; /bin/false ; } ; }
+
+	exitcode=0
+    rpmbuild_out="$(rpmbuild --rebuild ${RPMBUILD_OPTIONS} "${SRPMS_DIR}/${SRCRPM}" 2>&1)" || { exitcode="$?" ; /bin/true ; }
+    if [ "${exitcode}" -ne 0 ]; then
+			if [ "bashonfail" == "${BASH_ON_FAIL}" ]; then
+				# if the build is interactive, we can see what's printed in the current log, no need to reprint.
+				echo "Build failed, spawning a shell. The build will terminate after such shell is closed."
+				/bin/bash
+			else 
+				echo -e "${rpmbuild_out}\n\nrpmbuild command failed."
+			fi
+		exit ${exitcode}
+	fi
+    
+	files="$(sed -n -e '/Checking for unpackaged file/,$p' <<< "${rpmbuild_out}" | grep 'Wrote:' | cut -d ':' -f 2)"
+	
+	exitcode=0
+    echo -e "\n" | setsid rpmsign --addsign ${files} || { exitcode="$?" ; /bin/true ; }
+    if [ "${exitcode}" -ne 0 ]; then
+			if [ "bashonfail" == "${BASH_ON_FAIL}" ]; then
+				# if the build is interactive, we can see what's printed in the current log, no need to reprint.
+				echo "Signing failed, spawning a shell. The build will terminate after such shell is closed."
+				/bin/bash
+			else 
+				echo -e "${rpmbuild_out}\n\nrpmsign command failed."
+			fi
+		# don't accidentally retain unsigned files
+		rm -f ${files}
+		exit ${exitcode}
+	fi
 else
-    rpmbuild --rebuild ${RPMBUILD_OPTIONS} "${SRPMS_DIR}/${SRCRPM}" || { [ "bashonfail" == "$BASH_ON_FAIL" ] && { echo "Build failed, spawning a shell" ; /bin/bash ; exit 1; } || /bin/false ; }
+    rpmbuild --rebuild ${RPMBUILD_OPTIONS} "${SRPMS_DIR}/${SRCRPM}" || { [ "bashonfail" == "$BASH_ON_FAIL" ] && { echo "Build failed, spawning a shell" ; /bin/bash ; exit 1; } || exit 1 ; }
 fi
 
 echo "Done"
