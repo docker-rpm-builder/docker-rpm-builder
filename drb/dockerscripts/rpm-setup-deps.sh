77c23ca3a7bca4ec09936bfde7cbba8b54c8cc5f
Alan Franzoni <username@franzoni.eu>
2017-10-17 17:53:08 +0200

    Enh: better logging and shell fixes

--- drb/dockerscripts/rpm-setup-deps.sh
+++ drb/dockerscripts/rpm-setup-deps.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -ex
-echo "starting $0"
-SPECS_DIR=$(rpm --eval %{_specdir})
-SPEC=$(ls ${SPECS_DIR}/*.spec | head -n 1)
-yum-builddep -y --nogpgcheck ${SPEC}
+
+function log {
+    echo "[$(date --rfc-3339=seconds)]: $*"
+}
+
+EXIT_STATUS="FAIL"
+
+function finish {
+  log "${0}: exiting. Outcome: ${EXIT_STATUS}"
+}
+trap finish EXIT
+
+log "${0}: starting"
+SPECS_DIR="$(rpm --eval %\{_specdir\})"
+SPEC="$(ls "${SPECS_DIR}/*.spec" | head -n 1)"
+yum-builddep -y --nogpgcheck "${SPEC}"
+
+EXIT_STATUS="SUCCESS"
